40a8354aed5f6c601b4df6edf016213d128964d5
Nripesh Pradhan <pradhann@users.noreply.github.com>
2024-02-12 12:46:30 -0800

    header text in contacts

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -27,6 +27,14 @@ const socials = [
 export default function Contact() {
   return (
     <MainLayout>
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5 ">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+          Contact
+        </h1>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          Reach out if you have any questions or just want to say hi!
+        </p>
+      </div>
       <div className="socials-grid">
         {socials.map(({ Icon, href, label, handle }) => (
           <div key={label} className={`social-item`}>
