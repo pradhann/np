3863ac1826c6a485d7fd6b18f8d652aa1a1a7bae
Nripesh Pradhan <pradhann@users.noreply.github.com>
2024-02-12 00:37:44 -0800

    remove uses

--- content/headerNavLinks.ts
+++ content/headerNavLinks.ts
@@ -1,8 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
-  { href: '/contact', title: 'Contact' },
-  { href: '/uses', title: 'Uses' },
+  { href: '/contact', title: 'Contact' }
 ];
 
 export default headerNavLinks;
