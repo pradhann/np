d0ce574e1a591cde408d23c306894e5d055116d9
Nripesh Pradhan <pradhann@users.noreply.github.com>
2024-02-23 22:22:09 -0800

    fix intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -46,19 +46,19 @@ export default function Intro() {
       <div className="mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center px-10 py-24 text-4xl font-semibold tracking-tight md:py-28 md:text-6xl lg:px-20 lg:py-3 lg:text-7xl">
         <div className="leading-[1.15]">
           <div className="introText" style={{ opacity: opacityForBlock(progress, 0) }}>
-            I love coding.
+            ML Engineer @ChipperCash
           </div>
           <span
             className="introText inline-block after:content-['_']"
             style={{ opacity: opacityForBlock(progress, 1) }}
           >
-            I use my passion and skills to build digital products and experiences.
+            I talk to machines and make them talk back.
           </span>
           <span
             className="introText inline-block"
             style={{ opacity: opacityForBlock(progress, 2) }}
           >
-            I&apos;m passionate about cutting-edge, pixel perfect UI and intuitively implemented UX.
+            I&apos;m passionate about data, fintech, chess, poker and soccer.
           </span>
         </div>
       </div>
